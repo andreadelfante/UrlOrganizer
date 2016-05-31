c29372199a50c446ecaf0d9f02919ac76f3c01ee
fabiana <fabiana.lan@hotmail.it>
2016-05-31 13:49:43 +0200

    first commit

---
+++ src/UrlsEmbedding.py
@@ -0,0 +1,112 @@
+import os.path
+import numpy as np
+from enum import Enum
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+import hdbscan
+from sklearn import metrics
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from time import time
+
+_author_ = 'fabianalanotte'
+
+
+class Scale(Enum):
+    zscore = 1
+    minmax = 2
+    none = 3
+
+
+class Clustering_algorithm(Enum):
+    KMeans = 1
+    HDBscan = 2
+
+
+class UrlsEmbedding:
+    def __init__(self, filename, scaling=Scale.zscore):
+        assert isinstance(scaling, Scale), "the scaling parameter is not of type Scale"
+        self.embeddings, self.urls = self.__read_embeddings(filename)
+        self.normalized_embeddings = self.scale(self.embeddings, scaling)
+
+    def __read_embeddings(self, filename):
+        assert os.path.isfile(filename), "the file %r does not exist" % filename
+        in_file = open(filename, "r")
+        text = in_file.readlines()
+        urls = []
+        matrix = []
+
+        for line in text:
+            tokens = line.rstrip().split(' ', 1)
+            url = tokens[0]
+            embedding = np.fromstring(tokens[1], dtype=float, sep=' ')
+            matrix.append(embedding)
+            urls.append(url)
+
+        in_file.close()
+        return urls, matrix
+
+    def __scale(self, embeddings, type_scale):
+        if type_scale == Scale.zscore:
+            print('scaling embeddings with z score')
+            return self.__scaling_minmax(embeddings)
+        if type_scale == Scale.minmax:
+            print('scaling embeddings with minMaz normalization')
+            return self.__scaling_zscore(embeddings)
+        print('No scaling executed')
+        return embeddings
+
+    def __scaling_minmax(self, embeddings):
+        min_max_scaler = preprocessing.MinMaxScaler()
+        minmax_scale = min_max_scaler.fit_transform(embeddings)
+        return minmax_scale
+
+    def __scaling_zscore(self, embeddings):
+        standard_scale = preprocessing.scale(embeddings)
+        return standard_scale
+
+    def clustering(self, type_clustering=Clustering_algorithm.KMeans, n_clusters=10):
+        assert isinstance(type_clustering,
+                          Clustering_algorithm), "the input parameter is not of type Clustering_algorithm"
+        if type_clustering == Clustering_algorithm.KMeans:
+            print("Start running KMeans")
+            estimator = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+            estimator.fit(self.normalized_embeddings)
+            return estimator.labels_
+
+        print("Start running HDBscan")
+        estimator = hdbscan.HDBSCAN(min_cluster_size=4)
+        return estimator.fit_predict(self.normalized_embeddings)
+
+    def test(self, real_lables, learned_labels):
+        print('homo compl   v-meas  ARI AMI')
+        print('%.3f   %.3f   %.3f   %.3f   %.3f',
+              metrics.homogeneity_score(real_lables, learned_labels),
+              metrics.completeness_score(real_lables, learned_labels),
+              metrics.v_measure_score(real_lables, learned_labels),
+              metrics.adjusted_rand_score(real_lables, learned_labels),
+              metrics.adjusted_mutual_info_score(real_lables, learned_labels),
+              metrics.silhouette_score(self.normalized_embeddings, learned_labels,
+                                       metric='euclidean'))
+
+    def get_original_embedding(self):
+        return self.embeddings
+
+    def get_scaled_embeddings(self):
+        return self.normalized_embeddings
+
+    def plot_original_data(self):
+        model = TSNE(n_components=2, random_state=0)
+        np.set_printoptions(suppress=True)
+        data = model.fit_transform(self.embeddings)
+        plt.plot(data[:, 0], data[:, 1], 'o')
+        output = "original_embeddings" + time() + ".png"
+        plt.savefig(output)
+
+    def plot_scaled_data(self):
+        model = TSNE(n_components=2, random_state=0)
+        np.set_printoptions(suppress=True)
+        data = model.fit_transform(self.normalized_embeddings)
+        plt.plot(data[:, 0], data[:, 1], 'o')
+        output = "scaled_embeddings" + time() + ".png"
+        plt.savefig(output)
---
+++ src/util/UrlConverter.py
@@ -0,0 +1,48 @@
+import os.path
+import sys
+
+_author_ = 'fabianalanotte'
+
+class UrlConverter:
+    def __init__(self, file_url_clusterLabel, file_url_codeUrl, separator):
+        assert os.path.isfile(file_url_clusterLabel), "the file %r does not exist" % file_url_clusterLabel
+        assert os.path.isfile(file_url_codeUrl), "the file %r does not exist" % file_url_codeUrl
+        self.map_codeUrl_clusteringLabel = self.__generate_map_codeUrl_label(file_url_clusterLabel, file_url_codeUrl, separator)
+
+    def __generate_map_codeUrl_label(self, file_url_clusterLabel, file_url_codeUrl, separator):
+        in_file1 = open(file_url_clusterLabel, "r")
+        text = in_file1.readlines()
+        map1 = [{line.rstrip().split(separator)[0]:line.rstrip().split(',')[1] } for line in text]
+        in_file1.close()
+
+        in_file2 = open(file_url_codeUrl, "r")
+        text = in_file2.readlines()
+        map2 = [{line.rstrip().split(separator)[0]:line.rstrip().split(',')[1] } for line in text]
+        in_file2.close()
+
+        map_code_label = {}
+        for url, code in map2.items():
+            label = map1[url]
+            map_code_label[code] = label
+
+        return map_code_label
+
+    def get_ordered_labels(self, listUrl):
+        res = map(lambda u:self.map_codeUrl_clusteringLabel[u], listUrl)
+        return res
+
+    def main(self, argv):
+        if len(argv)!=3:
+            print('Wrong number of arguments. Inserted ', len(sys.argv), ', required 3')
+            print('UrlConverted.py <filename hashmap (url -> code)> <filename hashmap (url -> cluster_label)> <separator>')
+            print('UrlConverted /path/to/urlsMap.txt /path/to/urlToMembership.txt ,')
+            sys.exit(2)
+        converter = UrlConverter(argv[0], argv[1], argv[2])
+        for k,v in converter.map_codeUrl_clusteringLabel:
+            print(k,v)
+
+    if __name__ == "__main__":
+        main(sys.argv[1:])
+
+
+
